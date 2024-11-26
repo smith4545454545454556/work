0be8930de488897e4674fed008719af12742220d
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-26 21:33:45 +0545

    data

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const data = async () => {
     const response = await axios({
-        url: "/api/jdata",
+        url: "/api/data",
         method: "POST",
         data: { name: "smith" }
     })
