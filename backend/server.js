a0d5b930bb22c9cf63c902ab1fe6a1416dcbb5c9
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-27 13:06:37 +0545

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express"
 import { dataModel } from "./model/data.js"
+import { connectDB } from "./config.js"
+import dotenv from "dotenv"
+dotenv.config()
 const app = express()
 app.use(express.json())
 // app.get("/", (req, res) => {
@@ -28,11 +31,15 @@ app.get("/api/jokes", (req, res) => {
 })
 app.post("/api/data", async (req, res) => {
     const { name } = req.body
-    console.log(name)
-    return res.status(200).json({ message: "Data received", name }); // Send success response
+    const user = new dataModel({
+        name: name
+    })
+    await user.save()
+    return res.status(200).json({ message: "Data received", user }); // Send success response
 
 })
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+app.listen(port, async () => {
+    await connectDB()
     console.log(`server is running at ${port}`)
 })
